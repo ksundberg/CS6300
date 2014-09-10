f256b3731c6a24cef3e734abab33351666274222
Kenneth Sundberg <kasundberg@gmail.com>
2014-09-09 23:06:08 -0600

    Add extention point for CFG based optimizations

--- Optimizations/Optimizer.cpp
+++ Optimizations/Optimizer.cpp
@@ -1,8 +1,17 @@
 #include "Optimizer.hpp"
 
-/*Add new optimizations here*/
+/*Add new AST based optimizations here*/
 std::shared_ptr<cs6300::Program>
 cs6300::optimizer(std::shared_ptr<cs6300::Program> original)
 {
   return original;
 }
+/*Add new control flow graph based optimizations here*/
+std::pair<std::shared_ptr<cs6300::BasicBlock>,
+          std::shared_ptr<cs6300::BasicBlock>>
+cs6300::optimizer(std::pair<std::shared_ptr<cs6300::BasicBlock>,
+                            std::shared_ptr<cs6300::BasicBlock>> original)
+{
+  return original;
+}
+
