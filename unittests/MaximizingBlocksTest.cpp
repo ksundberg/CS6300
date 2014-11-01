464abad15c9758016d4281961fc318e15df8e13e
Aaron Andrews <aaronandrews29@gmail.com>
2014-11-01 17:27:59 -0600

    Added unit test

--- unittests/MaximizingBlocksTest.cpp
+++ unittests/MaximizingBlocksTest.cpp
@@ -1,1 +1,52 @@
+#include "gtest/gtest.h"
+#include "../Optimizations/MaximizeBlocks/MaximizeBlocks.hpp"
+#include "../AST_INC/AST/ThreeAddressInstruction.hpp"
 
+TEST(maximizing_blocks_test, test1)
+{
+    auto emptyBlock = std::make_shared<cs6300::BasicBlock>();
+    auto b0 = std::make_shared<cs6300::BasicBlock>();
+    auto b1 = std::make_shared<cs6300::BasicBlock>();
+    auto b2 = std::make_shared<cs6300::BasicBlock>();
+    auto b3 = std::make_shared<cs6300::BasicBlock>();
+    auto b4 = std::make_shared<cs6300::BasicBlock>();
+    auto b5 = std::make_shared<cs6300::BasicBlock>();
+    auto b6 = std::make_shared<cs6300::BasicBlock>();
+
+    emptyBlock->jumpTo = b0;
+    b0->jumpTo = b1;
+    b1->jumpTo = b2;
+    b1->branchTo = b6;
+    b2->jumpTo = b3;
+    b3->jumpTo = b4;
+    b3->branchTo = b5;
+    b4->jumpTo = b5;
+    b5->jumpTo = b6;
+    b5->branchTo = b1;
+
+    b0->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue,1,0,0));
+
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 2, 1, 0));
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 3, 100, 0));
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::IsLess, 4, 2, 3));
+
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 5, 5, 0));
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::Modulo, 7, 2, 5));
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::StoreMemory, 7, 2, 0));
+
+    b3->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::WriteStr, 7, 0, 0));
+
+    b4->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::WriteStr, 7, 0, 0));
+
+    b5->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 9, 1, 0));
+    b5->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 10, 1, 0));
+    b5->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::Add, 11, 9, 10));
+    b5->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::StoreMemory, 11, 1, 0));
+
+    auto blocks = std::pair<std::shared_ptr<cs6300::BasicBlock>,
+            std::shared_ptr<cs6300::BasicBlock>>(b0, b5);
+    cs6300::maximizeBlocks(blocks);
+
+
+
+}
