bb82f80767fa45ee5d7294e2c46e5685cfc4b0ec
Aaron Andrews <aaronandrews29@gmail.com>
2014-11-01 20:37:36 -0600

    Added new test case for maximizing basic blocks

--- unittests/MaximizingBlocksTest.cpp
+++ unittests/MaximizingBlocksTest.cpp
@@ -2,7 +2,7 @@
 #include "../Optimizations/MaximizeBlocks/MaximizeBlocks.hpp"
 #include "../AST_INC/AST/ThreeAddressInstruction.hpp"
 
-TEST(maximizing_blocks_test, complex_merge)
+TEST(complex_merge, maximizing_blocks_test)
 {
     auto emptyBlock = std::make_shared<cs6300::BasicBlock>();
     auto b0 = std::make_shared<cs6300::BasicBlock>();
@@ -54,7 +54,7 @@ TEST(maximizing_blocks_test, complex_merge)
     EXPECT_EQ(b4, b2->jumpTo);
 }
 
-TEST(maximizing_blocks_test, simple_merge)
+TEST(simple_merge, maximizing_blocks_test)
 {
     auto emptyBlock = std::make_shared<cs6300::BasicBlock>();
     auto b0 = std::make_shared<cs6300::BasicBlock>();
@@ -82,4 +82,47 @@ TEST(maximizing_blocks_test, simple_merge)
 
     EXPECT_EQ(7, b0->instructions.size());
     EXPECT_EQ(nullptr, b0->jumpTo);
+}
+
+
+TEST(simple_merge_check_order, maximizing_blocks_test)
+{
+    auto emptyBlock = std::make_shared<cs6300::BasicBlock>();
+    auto b0 = std::make_shared<cs6300::BasicBlock>();
+    auto b1 = std::make_shared<cs6300::BasicBlock>();
+    auto b2 = std::make_shared<cs6300::BasicBlock>();
+
+    emptyBlock->jumpTo = b0;
+    b0->jumpTo = b1;
+    b1->jumpTo = b2;
+
+    b0->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue,1,0,0));
+
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 2, 1, 0));
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 3, 100, 0));
+    b1->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::IsLess, 4, 2, 3));
+
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::LoadValue, 5, 5, 0));
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::Modulo, 7, 2, 5));
+    b2->instructions.push_back(cs6300::ThreeAddressInstruction(cs6300::ThreeAddressInstruction::StoreMemory, 7, 2, 0));
+
+    auto blocks = std::pair<std::shared_ptr<cs6300::BasicBlock>,
+            std::shared_ptr<cs6300::BasicBlock>>(b0, b1);
+
+    EXPECT_EQ(1, b0->instructions.size());
+    EXPECT_NE(nullptr, b0->jumpTo);
+
+    cs6300::maximizeBlocks(blocks);
+
+    EXPECT_EQ(7, b0->instructions.size());
+    EXPECT_EQ(nullptr, b0->jumpTo);
+
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::LoadValue, b0->instructions[0].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::LoadValue, b0->instructions[1].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::LoadValue, b0->instructions[2].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::IsLess, b0->instructions[3].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::LoadValue, b0->instructions[4].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::Modulo, b0->instructions[5].op);
+    EXPECT_EQ(cs6300::ThreeAddressInstruction::StoreMemory, b0->instructions[6].op);
+
 }
